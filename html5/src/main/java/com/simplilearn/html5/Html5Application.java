e9654f357bdaf6853441be5034a0216039519a44
amdah <amit.dahiya.j@gmail.com>
2023-10-21 14:55:33 +0000

    html google map example added

---
+++ html5/src/main/java/com/simplilearn/html5/Html5Application.java
@@ -0,0 +1,13 @@
+package com.simplilearn.html5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Html5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Html5Application.class, args);
+	}
+
+}
---
+++ html5/src/test/java/com/simplilearn/html5/Html5ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.simplilearn.html5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Html5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
