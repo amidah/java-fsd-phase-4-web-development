b8cc39871750bb24de377a63be88f178f403a03d
amdah <amit.dahiya.j@gmail.com>
2023-10-29 15:03:07 +0000

    added javascript bom details

---
+++ html-javascript/src/main/java/com/simplilearn/htmljavascript/HtmlJavascriptApplication.java
@@ -0,0 +1,13 @@
+package com.simplilearn.htmljavascript;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HtmlJavascriptApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HtmlJavascriptApplication.class, args);
+	}
+
+}
---
+++ html-javascript/src/test/java/com/simplilearn/htmljavascript/HtmlJavascriptApplicationTests.java
@@ -0,0 +1,13 @@
+package com.simplilearn.htmljavascript;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HtmlJavascriptApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
